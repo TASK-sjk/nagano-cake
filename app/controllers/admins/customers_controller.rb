3ea264ad503711002cacac56a10d3342d2d8a8b1
kouji-071 <koujisoccer71@gmail.com>
2020-04-07 11:35:58 +0000

    finish category

--- app/controllers/admins/customers_controller.rb
+++ app/controllers/admins/customers_controller.rb
@@ -1,13 +1,24 @@
 class Admins::CustomersController < ApplicationController
   def index
+  	@customer = Customer.all
   end
 
   def edit
+  	@customer = current_customer
   end
 
   def show
+  	@customer = current_customer
   end
 
   def update
+  	customer = Customer.find(params[ :id])
+  	customer.update(customer_params)
+  	redirect_to admins_customers_path(customer.id)
+  end
+
+  private
+  def customer_params
+  	params.require(:customer).permit(:first_name, :last_name, :kana_first_name, :kana_last_name, :pastcode, :address, :phone_namber, :email)
   end
 end
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,19 +1,33 @@
 class CategoriesController < ApplicationController
   def index
+    @category = Category.new
+    @categories = Category.all
   end
 
   def create
+    @category = Category.new(params[ :id])
+    @category.save
+    redirect_to categories_path
   end
 
   def new
   end
 
   def edit
+    @category = Category.find(params[ :id])
   end
 
   def update
+    category = Category.find(params[ :id])
+    category.update(category_params)
+    redirect_to categories_path
   end
 
   def destroy
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name, :status)
   end
 end
