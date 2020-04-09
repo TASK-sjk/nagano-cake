d81983da39e8a40ee1d9c8c8f24fcdaa5c8175aa
kouji-071 <koujisoccer71@gmail.com>
2020-04-09 13:25:41 +0000

    finish addmin/customer

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,8 +1,13 @@
 class AddressesController < ApplicationController
   def index
+    @address = Address.new
+    @address = Address.all
   end
 
   def create
+    @address = Address.new(address_path)
+    @address.save
+    redirect_to addresses_path
   end
 
   def new
@@ -15,5 +20,10 @@ class AddressesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def address_params
+    params.require(:address).permit(:postcode, :place, :receiver)
   end
 end
